16ffc7c6d410c77ecf0defbcce337d2f2ee40082
Marat <goodzilla365@mail.ru>
2019-05-12 21:57:24 +0300

    Подключена интерактивная Yandex карта

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,14 @@
 var button = document.querySelector(".menu-toggle");
 var menu = document.querySelector(".main-nav");
+var sizeX = 50;
+var sizeY = 45;
+var coordXPin = 59.938631;
+var coordYPin = 30.323055;
+var coordXCenter = 59.938631;
+var coordYCenter = 30.323055;
+var iconImageOffsetX = -25;
+var iconImageOffsetY = -30;
+
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -11,3 +20,38 @@ button.addEventListener("click", function (evt) {
     button.classList.add("menu-toggle--active");
   }
 });
+
+
+ymaps.ready(init);
+
+if (window.innerWidth >= 768) {
+  sizeX = 100;
+  sizeY = 90;
+  iconImageOffsetX = -40;
+  iconImageOffsetY = -75;
+}
+
+if (window.innerWidth >= 1300) {
+  coordXCenter = 59.938521;
+  coordYCenter = 30.320595;
+}
+
+function init() {
+  myMap = new ymaps.Map("yandex-map", {
+      center: [coordXCenter, coordYCenter],
+      zoom: 17,
+      controls: ['smallMapDefaultSet']
+    }),
+
+    myPlacemark = new ymaps.Placemark([coordXPin, coordYPin], {
+      hintContent: 'Магазин спортивного питания Cat Energy'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-pin.png',
+      iconImageSize: [sizeX, sizeY],
+      iconImageOffset: [iconImageOffsetX, iconImageOffsetY]
+    });
+
+  myMap.geoObjects
+    .add(myPlacemark);
+};
