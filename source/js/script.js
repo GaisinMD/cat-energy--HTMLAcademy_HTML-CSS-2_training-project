902ba611bf23c2b17d6c3f803b59c1b793ba6600
Marat <goodzilla365@mail.ru>
2019-05-14 15:02:39 +0300

    Добавлено поведение формы JS

--- source/js/script.js
+++ source/js/script.js
@@ -8,8 +8,15 @@ var coordXCenter = 59.938631;
 var coordYCenter = 30.323055;
 var iconImageOffsetX = -25;
 var iconImageOffsetY = -30;
+var mainForm = document.querySelector(".main-form");
+var fieldCatName = document.querySelector("#cat-name");
+var fieldCatWeight = document.querySelector("#cat-weight");
+var fieldCatAge = document.querySelector("#cat-age");
+var fieldManMail = document.querySelector("#man-mail");
+var fieldManPhone = document.querySelector("#man-phone");
+var fieldCommentText = document.querySelector("#comment-text");
 
-
+//кнопка вызова меню в мобильной версии
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (button.classList.contains("menu-toggle--active")) {
@@ -21,7 +28,7 @@ button.addEventListener("click", function (evt) {
   }
 });
 
-
+//карта
 ymaps.ready(init);
 
 if (window.innerWidth >= 768) {
@@ -55,3 +62,49 @@ function init() {
   myMap.geoObjects
     .add(myPlacemark);
 };
+
+//поведение формы
+mainForm.addEventListener("submit", function (evt) {
+  if (!fieldCatName.value || !fieldCatWeight.value || !fieldCatAge.value || !fieldManMail.value || !fieldManPhone.value || !fieldCommentText.value) {
+    evt.preventDefault();
+    if (!fieldCatName.value) {
+      fieldCatName.classList.add("fieldset-cat__input--error");
+    }
+    if (!fieldCatWeight.value) {
+      fieldCatWeight.classList.add("fieldset-cat__input--error");
+    }
+    if (!fieldCatAge.value) {
+      fieldCatAge.classList.add("fieldset-cat__input--error");
+    }
+    if (!fieldManMail.value) {
+      fieldManMail.classList.add("fieldset-man__input--error");
+    }
+    if (!fieldManPhone.value) {
+      fieldManPhone.classList.add("fieldset-man__input--error");
+    }
+    if (!fieldCommentText.value) {
+      fieldCommentText.classList.add("fieldset-comment__text--error");
+    }
+  }
+});
+
+mainForm.addEventListener("keydown", function (evt) {
+  if (fieldCatName.value && fieldCatName.classList.contains("fieldset-cat__input--error")) {
+    fieldCatName.classList.remove("fieldset-cat__input--error");
+  }
+  if (fieldCatWeight.value && fieldCatWeight.classList.contains("fieldset-cat__input--error")) {
+    fieldCatWeight.classList.remove("fieldset-cat__input--error");
+  }
+  if (fieldCatAge.value && fieldCatAge.classList.contains("fieldset-cat__input--error")) {
+    fieldCatAge.classList.remove("fieldset-cat__input--error");
+  }
+  if (fieldManMail.value && fieldManMail.classList.contains("fieldset-man__input--error")) {
+    fieldManMail.classList.remove("fieldset-man__input--error");
+  }
+  if (fieldManPhone.value && fieldManPhone.classList.contains("fieldset-man__input--error")) {
+    fieldManPhone.classList.remove("fieldset-man__input--error");
+  }
+  if (fieldCommentText.value && fieldCommentText.classList.contains("fieldset-comment__text--error")) {
+    fieldCommentText.classList.remove("fieldset-comment__text--error");
+  }
+});
