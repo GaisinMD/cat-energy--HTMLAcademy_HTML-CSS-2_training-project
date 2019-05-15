43b391d21401a7a65a825a2cd267d4623d41cc94
Marat <goodzilla365@mail.ru>
2019-05-15 21:03:27 +0300

    Кроссбраузерность-3

--- source/js/script.js
+++ source/js/script.js
@@ -64,47 +64,49 @@ function init() {
 };
 
 //поведение формы
-mainForm.addEventListener("submit", function (evt) {
-  if (!fieldCatName.value || !fieldCatWeight.value || !fieldCatAge.value || !fieldManMail.value || !fieldManPhone.value || !fieldCommentText.value) {
-    evt.preventDefault();
-    if (!fieldCatName.value) {
-      fieldCatName.classList.add("fieldset-cat__input--error");
+if (mainForm) {
+  mainForm.addEventListener("submit", function (evt) {
+    if (!fieldCatName.value || !fieldCatWeight.value || !fieldCatAge.value || !fieldManMail.value || !fieldManPhone.value || !fieldCommentText.value) {
+      evt.preventDefault();
+      if (!fieldCatName.value) {
+        fieldCatName.classList.add("fieldset-cat__input--error");
+      }
+      if (!fieldCatWeight.value) {
+        fieldCatWeight.classList.add("fieldset-cat__input--error");
+      }
+      if (!fieldCatAge.value) {
+        fieldCatAge.classList.add("fieldset-cat__input--error");
+      }
+      if (!fieldManMail.value) {
+        fieldManMail.classList.add("fieldset-man__input--error");
+      }
+      if (!fieldManPhone.value) {
+        fieldManPhone.classList.add("fieldset-man__input--error");
+      }
+      if (!fieldCommentText.value) {
+        fieldCommentText.classList.add("fieldset-comment__text--error");
+      }
     }
-    if (!fieldCatWeight.value) {
-      fieldCatWeight.classList.add("fieldset-cat__input--error");
-    }
-    if (!fieldCatAge.value) {
-      fieldCatAge.classList.add("fieldset-cat__input--error");
-    }
-    if (!fieldManMail.value) {
-      fieldManMail.classList.add("fieldset-man__input--error");
-    }
-    if (!fieldManPhone.value) {
-      fieldManPhone.classList.add("fieldset-man__input--error");
-    }
-    if (!fieldCommentText.value) {
-      fieldCommentText.classList.add("fieldset-comment__text--error");
-    }
-  }
-});
+  });
 
-mainForm.addEventListener("keydown", function (evt) {
-  if (fieldCatName.value && fieldCatName.classList.contains("fieldset-cat__input--error")) {
-    fieldCatName.classList.remove("fieldset-cat__input--error");
-  }
-  if (fieldCatWeight.value && fieldCatWeight.classList.contains("fieldset-cat__input--error")) {
-    fieldCatWeight.classList.remove("fieldset-cat__input--error");
-  }
-  if (fieldCatAge.value && fieldCatAge.classList.contains("fieldset-cat__input--error")) {
-    fieldCatAge.classList.remove("fieldset-cat__input--error");
-  }
-  if (fieldManMail.value && fieldManMail.classList.contains("fieldset-man__input--error")) {
-    fieldManMail.classList.remove("fieldset-man__input--error");
-  }
-  if (fieldManPhone.value && fieldManPhone.classList.contains("fieldset-man__input--error")) {
-    fieldManPhone.classList.remove("fieldset-man__input--error");
-  }
-  if (fieldCommentText.value && fieldCommentText.classList.contains("fieldset-comment__text--error")) {
-    fieldCommentText.classList.remove("fieldset-comment__text--error");
-  }
-});
+  mainForm.addEventListener("keydown", function (evt) {
+    if (fieldCatName.value && fieldCatName.classList.contains("fieldset-cat__input--error")) {
+      fieldCatName.classList.remove("fieldset-cat__input--error");
+    }
+    if (fieldCatWeight.value && fieldCatWeight.classList.contains("fieldset-cat__input--error")) {
+      fieldCatWeight.classList.remove("fieldset-cat__input--error");
+    }
+    if (fieldCatAge.value && fieldCatAge.classList.contains("fieldset-cat__input--error")) {
+      fieldCatAge.classList.remove("fieldset-cat__input--error");
+    }
+    if (fieldManMail.value && fieldManMail.classList.contains("fieldset-man__input--error")) {
+      fieldManMail.classList.remove("fieldset-man__input--error");
+    }
+    if (fieldManPhone.value && fieldManPhone.classList.contains("fieldset-man__input--error")) {
+      fieldManPhone.classList.remove("fieldset-man__input--error");
+    }
+    if (fieldCommentText.value && fieldCommentText.classList.contains("fieldset-comment__text--error")) {
+      fieldCommentText.classList.remove("fieldset-comment__text--error");
+    }
+  });
+}
