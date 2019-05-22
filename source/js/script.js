2907c903761d33d722ec686a3f496fc81e5167a6
Marat <goodzilla365@mail.ru>
2019-05-22 13:38:24 +0300

    Б30

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var button = document.querySelector(".menu-toggle");
 var menu = document.querySelector(".main-nav");
 
 menu.classList.remove("main-nav--show");
+button.classList.remove("menu-toggle--hidden");
 
 //кнопка вызова меню в мобильной версии
 button.addEventListener("click", function (evt) {
